86a085b068c2342bc288e5207c555fe31450efd7
Cristhian Nasimba <mcnasimba@uce.edu.ec>
2022-12-13 05:36:10 -0500

    Store

--- store/src/app/app.component.ts
+++ store/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Stores2';
+
+  getName (): void {
+    console.log('Hola Mundo')
+  }
 }
