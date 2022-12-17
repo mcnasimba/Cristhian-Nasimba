d405b8a13a67da0e052673abd718b8e3d221f3cd
Cristhian Nasimba <mcnasimba@uce.edu.ec>
2022-12-16 21:30:09 -0500

    SICPA

---
+++ sistema-sicpa-backend/src/main/java/com/sistema/sicpa/SistemaSicpaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.sicpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSicpaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSicpaBackendApplication.class, args);
+	}
+
+}
